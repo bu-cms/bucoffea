4e3830a9765555a2b4e2db45a5c8c827c0c2577b
albert <albert@physik.rwth-aachen.de>
2019-06-07 21:44:09 +0200

    Rename

--- bucoffea/executors/htcondor.py
+++
@@ -1,7 +0,0 @@
-import htcondor
-
-def htcondor_executor(items, function, accumulator, status=True, unit='items', desc='Processing',
-                       function_args={}):
-    for i, item in tqdm(enumerate(items), disable=not status, unit=unit, total=len(items), desc=desc):
-        accumulator += function(item, **function_args)
-    return accumulator
