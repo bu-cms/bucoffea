5eec755d7343bc912f55cae71b9e6a2f51771d58
Andreas Albert <andreas.albert@cern.ch>
2019-06-25 14:47:14 -0400

    Reenable tau veto

--- bucoffea/monojet/monojetProcessor.py
+++ bucoffea/monojet/monojetProcessor.py
@@ -80,7 +80,7 @@ class monojetProcessor(processor.ProcessorABC):
         selection.add('veto_ele', electrons.counts==0)
         selection.add('veto_muo', muons.counts==0)
         selection.add('veto_photon', photons.counts==0)
-        selection.add('veto_tau', np.ones(df.size)==1)#taus.counts==0)
+        selection.add('veto_tau', taus.counts==0)
         selection.add('veto_b',np.ones(df.size)==1) #bjets.counts==0)
         selection.add('dphijm',df['minDPhiJetMet'] > cfg.SELECTION.SIGNAL.MINDPHIJM)
         selection.add('dpfcalo',np.abs(df['dPFCalo']) < cfg.SELECTION.SIGNAL.DPFCALO)
