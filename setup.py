6f0960893c1f5aec575dbb04f8111852792f5de9
Andreas Albert <andreas.albert@cern.ch>
2019-06-25 09:09:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name = 'bucoffea',
     version = '0.0.1',
-    url = 'https://github.com/AndreasAlbert/monojetcoffea.git',
+    url = 'https://github.com/bu-cms/bucoffea',
     author = 'Andreas Albert',
     author_email = 'andreas.albert@cern.ch',
-    description = 'Monojet analysis using Coffea on NanoAOD',
+    description = 'Analysis using Coffea on NanoAOD',
     packages = find_packages(),    
     install_requires = ['coffea'],
 )
