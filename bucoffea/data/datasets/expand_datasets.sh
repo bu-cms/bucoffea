019595c5dccfb54857373e2b34d483dcee33e769
Andreas Albert <andreas.albert@cern.ch>
2019-07-01 16:53:08 -0400

    Add dataset list

---
+++ bucoffea/data/datasets/expand_datasets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+expand(){
+    INFILE=${1}
+    COND=${2}
+    for STUB in $(cat ${INFILE}); do
+        dasgoclient --query="dataset=/${STUB}/${COND}/NANOAOD*"
+    done
+}
+
+INFILE="datasetnames.txt"
+expand "${INFILE}" "RunIIFall17*1June*" | tee datasets_mc_2017.txt
+expand "${INFILE}" "RunIIAutumn18*1June*" | tee datasets_mc_2018.txt
