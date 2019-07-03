dccb349e50f380da824c1271d31e77b57025c6fe
Andreas Albert <andreas.albert@cern.ch>
2019-07-03 12:16:19 -0400

    Update dataset tables

--- bucoffea/data/datasets/expand_datasets.sh
+++ bucoffea/data/datasets/expand_datasets.sh
@@ -4,11 +4,23 @@
 expand(){
     INFILE=${1}
     COND=${2}
-    for STUB in $(cat ${INFILE}); do
-        dasgoclient --query="dataset=/${STUB}/${COND}/NANOAOD*"
-    done
+    # for STUB in $(cat ${INFILE}); do
+    while read STUB; do
+        if [[ $STUB = \#* ]]; then
+            echo $STUB
+        elif [ -z "${STUB}" ]; then
+            echo ""
+        else
+            # echo $STUB
+            dasgoclient --query="dataset=/${STUB}/${COND}/NANOAOD*"
+        fi
+    done < ${INFILE}
 }
 
-INFILE="datasetnames.txt"
+INFILE="dataset_names_mc.txt"
 expand "${INFILE}" "RunIIFall17*1June*" | tee datasets_2017.txt
 expand "${INFILE}" "RunIIAutumn18*1June*" | tee datasets_2018.txt
+
+INFILE="dataset_names_data.txt"
+expand "${INFILE}" "Run2017*1June*" | tee -a datasets_2017.txt
+expand "${INFILE}" "Run2018*1June*" | tee -a datasets_2018.txt
