0d91832f4d383ccee487c6ded1ab95a2b314a74a
albert <albert@physik.rwth-aachen.de>
2019-07-06 18:12:45 +0200

    Update htcondor wrap

--- bucoffea/execute/htcondor_wrap.sh
+++ bucoffea/execute/htcondor_wrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 echo "Starting: $(date)"
 source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc8-opt/setup.sh
 
@@ -6,7 +7,6 @@ ARGS=("$@")
 echo "Arguments: " ${ARGS[@]}
 echo "Initiating VOMS proxy."
 export X509_USER_PROXY=${1}
-voms-proxy-info -all
 voms-proxy-info -all -file ${1}
 
 if [ ! -z "${VIRTUAL_ENV}" ]; then
@@ -15,6 +15,33 @@ if [ ! -z "${VIRTUAL_ENV}" ]; then
 fi
 echo "Using python at: $(which python)"
 
-time ${ARGS[@]:1}
+# Copy files to local disk before running
+PREFETCH=true
+if $PREFETCH; then
+    echo "Prefetching."
+    FLIST=$(readlink -e input*.txt)
+    touch tmp.txt
+    while read file; do
+        LOCAL=$(echo "${file}" | md5sum | awk '{print $1}').root
+        xrdcp $file ./$LOCAL
+        echo $LOCAL >> tmp.txt
+    done < FLIST
+    mv tmp.txt $FLIST
+fi
 
-echo "Ending: $(date)"
+executable=${2}
+cp -v ${executable} .
+
+echo "Directory content---"
+ls -lah .
+echo "===================="
+
+
+echo "Setup done: $(date)"
+time python $(basename ${executable}) ${ARGS[@]:2}
+echo "Run done: $(date)"
+
+echo "Cleaning up."
+rm -v *.root
+
+echo "End: $(date)"
