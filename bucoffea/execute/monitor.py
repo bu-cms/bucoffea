30abec7e9ea8844fc16660a3b8d714b22a58a820
albert <albert@physik.rwth-aachen.de>
2019-07-17 19:35:11 +0200

    Improve condor monitoring script

--- bucoffea/execute/monitor.py
+++ bucoffea/execute/monitor.py
@@ -6,6 +6,17 @@ from htcondor import JobEventType
 import os
 from collections import defaultdict
 from tabulate import tabulate
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    WHITE = '\033[97m'
+    GRAY = '\033[90m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 directories = sys.argv[1:]
 
@@ -16,7 +27,7 @@ for directory in directories:
 jels = {}
 for log in logs:
     name = os.path.basename(log).replace("log_","").replace(".txt","")
-    jels = { name : htcondor.JobEventLog(log) for log in logs}
+    jels[name] = htcondor.JobEventLog(log)
 
 while True:
     try:
@@ -25,10 +36,19 @@ while True:
             for event in j.events(stop_after=0):
                 latest = event
             try:
-                ret = event["ReturnValue"]
+                ret = latest["ReturnValue"]
             except KeyError:
                 ret = "-"
-            table.append([log, latest.cluster, str(JobEventType.values[latest.type]) ])
+            col = ""
+
+            if ret == 0:
+                log = bcolors.OKGREEN + log
+                ret = f"{ret}" + bcolors.ENDC
+            elif ret != "-":
+                log = bcolors.FAIL + log
+                ret = f"{ret}" + bcolors.ENDC
+
+            table.append([log, latest.cluster, str(JobEventType.values[latest.type]), ret ])
         print(tabulate(table, headers=["Name", "Cluster", "Status", "Return"]))
     except KeyboardInterrupt:
         break
