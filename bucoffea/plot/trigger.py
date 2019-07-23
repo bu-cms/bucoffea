Fix MC normalization.

bucoffea/plot/trigger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from bucoffea.plot.util import acc_from_dir, merge_datasets, merge_extensions, normalize_mc, fig_ratio, lumi
+from bucoffea.plot.util import acc_from_dir, merge_datasets, merge_extensions, scale_xs_lumi, fig_ratio, lumi
 from bucoffea.plot.style import markers
 from coffea import hist
 from coffea.hist.plot import clopper_pearson_interval
@@ -28,35 +28,36 @@ def trgname(year, tag):
 
 xmax = 1e3
 
-def content_table(hnum, hden):
+def content_table(hnum, hden, axis_name):
     table = []
-    axis = 'recoil'
-    try:
-        hnum.axis(axis)
-    except KeyError:
-        axis = 'met'
-    for x,ynum, yden in zip(hnum.axis(axis).identifiers(),hnum.values()[()],hden.values()[()]):
+    for x,ynum, yden in zip(hnum.axis(axis_name).identifiers(),hnum.values()[()],hden.values()[()]):
         eff =  ynum/ yden if yden != 0 else 0
         unc = clopper_pearson_interval(ynum, yden, 0.68)
         line = [(x.lo + x.hi)/2, ynum, yden, eff,unc[0], unc[1]]
         table.append(line)
     return tabulate(table, headers=['Recoil', 'Numerator', 'Denominator',"Efficiency", "Eff-sigma","Eff+sigma"])
 
-def plot_recoil(acc, region_tag="1m", dataset='SingleMuon', year=2018, tag="test", distribution="recoil"):
+def plot_recoil(acc, region_tag="1m", dataset='SingleMuon', year=2018, tag="test", distribution="recoil",axis_name=None):
+    # Select and prepare histogram
     h = copy.deepcopy(acc[distribution])
-    h = merge_extensions(h)
-    normalize_mc(h, acc)
+    h = merge_extensions(h, acc)
+    scale_xs_lumi(h)
     h = merge_datasets(h)
 
-    newbin = hist.Bin(distribution,f"{distribution} (GeV)",np.array(list(range(0,400,20)) + list(range(400,1100,100))))
-    h = h.rebin(h.axis(distribution), newbin)
+    # Rebinning
+    axis_name = distribution if not axis_name else axis_name
+    newbin = hist.Bin(axis_name,f"{axis_name} (GeV)",np.array(list(range(0,400,20)) + list(range(400,1100,100))))
+    h = h.rebin(h.axis(axis_name), newbin)
     ds = f'{dataset}_{year}'
-    h = h.project(h.axis('dataset'), ds)
-    # print(h)
-    hnum = h.project(h.axis('region'),f'tr_{region_tag}_num')
-    hden = h.project(h.axis('region'),f'tr_{region_tag}_den')
 
-    # print(hden, hnum)
+    # Pick dataset and regions
+    h = h.project(h.axis('dataset'), ds)
+    if region_tag == 'g':
+        hnum = h.project(h.axis('region'),f'tr_{region_tag}_num_cuts')
+        hden = h.project(h.axis('region'),f'tr_{region_tag}_den_cuts')
+    else:
+        hnum = h.project(h.axis('region'),f'tr_{region_tag}_num')
+        hden = h.project(h.axis('region'),f'tr_{region_tag}_den')
 
     # Recoil plot
     try:
@@ -72,10 +73,10 @@ def plot_recoil(acc, region_tag="1m", dataset='SingleMuon', year=2018, tag="test
         os.makedirs(outdir)
     fig.savefig(pjoin(outdir, f'{distribution}_{region_tag}_{dataset}_{year}.pdf'))
     with open(pjoin(outdir,f'table_{region_tag}_{dataset}_{year}.txt'),"w") as f:
-        f.write(content_table(hnum, hden) + "\n")
+        f.write(content_table(hnum, hden, axis_name) + "\n")
     plt.close(fig)
 
-    # Efficiency
+    # Efficiency plot
     fig, ax,_ = hist.plotratio(hnum, hden,
                 guide_opts={},
                 unc='clopper-pearson',
@@ -266,37 +267,47 @@ def sf_comparison_plot(tag):
         fig.clear()
         plt.close(fig)
 
+def met_triggers():
+        tag = '120pfht_hltmu'
+        indir = f"/home/albert/repos/bucoffea/bucoffea/plot/input/eff/{tag}"
+        acc = acc_from_dir(indir)
 
+        for year in [2017, 2018]:
+            region = '1m'
+            for dataset in ["WJetsToLNu-MLM", "SingleMuon"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
+            region = '2m'
+            for dataset in ["DYNJetsToLL_M-50-MLM", "SingleMuon"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
+            region = '1m_hlt'
+            for dataset in ["WJetsToLNu-MLM", "SingleMuon"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
+            region = '2m_hlt'
+            for dataset in ["DYNJetsToLL_M-50-MLM", "SingleMuon"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
+            region = '1e'
+            for dataset in ["WJetsToLNu-MLM", "EGamma"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag, distribution='met')
+            region = '2e'
+            for dataset in ["DYNJetsToLL_M-50-MLM", "EGamma"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag, distribution='met')
 
+        region_comparison_plot(tag)
+        sf_comparison_plot(tag)
 
-def main():
-    # indir = "/home/albert/repos/bucoffea/bucoffea/plot/input/eff/test"
-
-    tag = '120pfht_hltmu'
+def photon_triggers():
+    tag = 'gamma'
     indir = f"/home/albert/repos/bucoffea/bucoffea/plot/input/eff/{tag}"
     acc = acc_from_dir(indir)
 
     for year in [2017, 2018]:
-        region = '1m'
-        for dataset in ["WJetsToLNu-MLM", "SingleMuon"]:
-            plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
-        region = '2m'
-        for dataset in ["DYNJetsToLL_M-50-MLM", "SingleMuon"]:
-            plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
-        region = '1m_hlt'
-        for dataset in ["WJetsToLNu-MLM", "SingleMuon"]:
-            plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
-        region = '2m_hlt'
-        for dataset in ["DYNJetsToLL_M-50-MLM", "SingleMuon"]:
-            plot_recoil(acc,region,dataset=dataset,year=year, tag=tag)
-        region = '1e'
-        for dataset in ["WJetsToLNu-MLM", "EGamma"]:
-            plot_recoil(acc,region,dataset=dataset,year=year, tag=tag, distribution='met')
-        region = '2e'
-        for dataset in ["DYNJetsToLL_M-50-MLM", "EGamma"]:
-            plot_recoil(acc,region,dataset=dataset,year=year, tag=tag, distribution='met')
+            region = 'g'
+            for dataset in ["GJets_HT_MLM", "JetHT"]:
+                plot_recoil(acc,region,dataset=dataset,year=year, tag=tag, distribution='photonpt0',axis_name='pt')
+def main():
+    # indir = "/home/albert/repos/bucoffea/bucoffea/plot/input/eff/test"
+    photon_triggers()
 
-    region_comparison_plot(tag)
-    sf_comparison_plot(tag)
+
 if __name__ == "__main__":
     main()