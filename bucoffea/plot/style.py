eccbad340fa42847c477570705ac5d62f7050dd1
Andreas Albert <andreas.albert@cern.ch>
2019-08-14 13:06:30 -0400

    Add matplotlib rc to style

--- bucoffea/plot/style.py
+++ bucoffea/plot/style.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 
 def markers(tag):
     if tag =='data':
@@ -10,3 +11,10 @@ def markers(tag):
             'emarker': '_'
         }
     return ret
+
+def matplotlib_rc():
+    plt.rc('mathtext',rm='Helvetica')
+    plt.rc('mathtext',it='Helvetica')
+    plt.rc('mathtext',bf='Helvetica')
+    params = {'font.size':14, 'lines.linewidth' : 1}
+    plt.rcParams.update(params)
