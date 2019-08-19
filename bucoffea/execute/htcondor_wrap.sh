9eabab4ebf1a9f5a877a5597b66213ad95f4f41a
Andreas Albert <andreas.albert@cern.ch>
2019-08-19 09:36:31 -0500

    Implement FNAL eos

--- bucoffea/execute/htcondor_wrap.sh
+++ bucoffea/execute/htcondor_wrap.sh
@@ -5,6 +5,8 @@ if [ "$BUCOFFEADEBUG" = true ]; then
 fi
 echo "Starting: $(date)"
 source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc8-opt/setup.sh
+echo "Using python at: $(which python)"
+python --version
 
 ARGS=("$@")
 
@@ -16,8 +18,16 @@ fi
 if [ ! -z "${VIRTUAL_ENV}" ]; then
     echo "Found VIRTUAL_ENV variable."
     source ${VIRTUAL_ENV}/bin/activate
+    cp "${BUCOFFEAEXECUTABLE}" .
+    executable=$(basename "${BUCOFFEAEXECUTABLE}")
+else
+    tar xf *tgz
+    rm -rvf *tgz
+    ENVNAME="bucoffeaenv"
+    python -m venv ${ENVNAME}
+    source ${ENVNAME}/bin/activate
+    python -m pip install -e bucoffea --no-cache-dir
 fi
-echo "Using python at: $(which python)"
 
 # Copy files to local disk before running
 if [ ! "$NOPREFETCH" = true ]; then
@@ -32,21 +42,17 @@ if [ ! "$NOPREFETCH" = true ]; then
     mv tmp.txt $FLIST
 fi
 
-executable=${1}
-cp -v ${executable} .
-
 echo "Directory content---"
 ls -lah .
 echo "===================="
 
 
 echo "Setup done: $(date)"
-time python $(basename ${executable}) ${ARGS[@]:1}
+time buexec ${ARGS[@]}
 echo "Run done: $(date)"
 
 echo "Cleaning up."
 rm -vf *.root
 rm -vf ${FLIST}
-rm -vf $(basename $executable)
 echo "End: $(date)"
 
