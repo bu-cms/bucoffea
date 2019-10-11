2d5588b9482b51783664e5e94bc9f5aeb04d8165
Alp Akpinar <aakpinar@bu.edu>
2019-10-11 09:48:59 -0500

    Added linear y-axis plotting option

--- bucoffea/scripts/print_plots.py
+++ bucoffea/scripts/print_plots.py
@@ -9,13 +9,14 @@ def commandline():
     parser.add_argument('file', type=str, help='Input file to use.')
     parser.add_argument('--outpath', type=str, help='Path to save output under.', default='./out')
     parser.add_argument('--region', type=str, default='inclusive', help='Region to plot.')
+    parser.add_argument('--logscaley', action='store_true', help='Plot y axis logarithmically.')
     args = parser.parse_args()
     return args
 
 def main():
     args = commandline()
     acc = load(args.file)
-    debug_plot_output(acc, args.region, args.outpath)
+    debug_plot_output(acc, args.region, args.outpath, args.logscaley)
 
 if __name__ == "__main__":
     main()
