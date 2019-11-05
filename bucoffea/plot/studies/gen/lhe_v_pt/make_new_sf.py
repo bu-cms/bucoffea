7641e7fc07dac65f7c5570861434387ee4f3173e
Andreas Albert <andreas.albert@cern.ch>
2019-11-05 17:12:43 -0500

    2D NLO SF

--- bucoffea/plot/studies/gen/lhe_v_pt/make_new_sf.py
+++ bucoffea/plot/studies/gen/lhe_v_pt/make_new_sf.py
@@ -39,55 +39,56 @@ def sf_1d(acc, tag, regex, outputrootfile):
     # new_ax = hist.Bin('vpt','LHE V $p_{T}$ (GeV)',list(range(100,500,50)) + list(range(500,1000,100)) + list(range(1000,2000,250)))
     new_ax = hist.Bin('vpt','V $p_{T}$ (GeV)',list(range(100,800,100))+list(range(800,1200,200))+list(range(1200,2800,800)))
 
-    for selection in ['inclusive','monojet','vbf']:
-        dist = f'gen_vpt_{selection}'
-        h = copy.deepcopy(acc[dist])
+    for calc in ['dress','stat1']:
+        for selection in ['inclusive','monojet','vbf']:
+            dist = f'gen_vpt_{selection}_{calc}'
+            h = copy.deepcopy(acc[dist])
 
-        h = h.rebin(h.axis('vpt'), new_ax)
+            h = h.rebin(h.axis('vpt'), new_ax)
 
-        if selection == 'monojet':
-            h = h.integrate(h.axis("jpt"))
-        if selection == 'vbf':
-            h = h.integrate(h.axis("jpt"))
-            h = h.integrate(h.axis("mjj"))
-        h = merge_extensions(h, acc, reweight_pu=False)
-        scale_xs_lumi(h)
-        h = merge_datasets(h)
-        h = h[re.compile(regex)]
-        hist.plot1d(
-            h,
-            overlay='dataset',
-            overflow='all',
-            binwnorm=True,
-            ax=ax)
-        lo = h[re.compile('.*HT.*')].integrate('dataset')
-        nlo = h[re.compile('.*LHE.*')].integrate('dataset')
+            if selection == 'monojet':
+                h = h.integrate(h.axis("jpt"))
+            if selection == 'vbf':
+                h = h.integrate(h.axis("jpt"))
+                h = h.integrate(h.axis("mjj"))
+            h = merge_extensions(h, acc, reweight_pu=False)
+            scale_xs_lumi(h)
+            h = merge_datasets(h)
+            h = h[re.compile(regex)]
+            hist.plot1d(
+                h,
+                overlay='dataset',
+                overflow='all',
+                binwnorm=True,
+                ax=ax)
+            lo = h[re.compile('.*HT.*')].integrate('dataset')
+            nlo = h[re.compile('.*LHE.*')].integrate('dataset')
 
-        hist.plotratio(nlo, lo,
-            ax=rax,
-            denom_fill_opts={},
-            guide_opts={},
-            unc='num',
-            overflow='all',
-            error_opts=data_err_opts,
-            label='2017 NLO/LO ratio'
-            )
-        old = get_old_kfac(tag)
-        old_x = 0.5*(old.bins[:,0]+old.bins[:,1])
-        rax.plot(old_x, old.values,'ob-', label='2016 QCD k fac')
-        rax.plot(old_x, old.values * pdfwgt_sf(old_x),'or-', label='2016 x ad-hoc DY pdfwgt SF')
-        ax.set_yscale('log')
-        ax.set_ylim(1e-3,1e6)
-        rax.set_ylim(0,2)
-        rax.legend()
+            hist.plotratio(nlo, lo,
+                ax=rax,
+                denom_fill_opts={},
+                guide_opts={},
+                unc='num',
+                overflow='all',
+                error_opts=data_err_opts,
+                label='2017 NLO/LO ratio'
+                )
+            old = get_old_kfac(tag)
+            old_x = 0.5*(old.bins[:,0]+old.bins[:,1])
+            rax.plot(old_x, old.values,'ob-', label='2016 QCD k fac')
+            rax.plot(old_x, old.values * pdfwgt_sf(old_x),'or-', label='2016 x ad-hoc DY pdfwgt SF')
+            ax.set_yscale('log')
+            ax.set_ylim(1e-3,1e6)
+            rax.set_ylim(0,2)
+            rax.legend()
 
 
-        fig.savefig(pjoin(outdir,f'{tag}_{dist}.pdf'))
+            fig.savefig(pjoin(outdir,f'{tag}_{dist}.pdf'))
 
-        sf_x = lo.axis('vpt').edges()
-        sf_y = nlo.values()[()] / lo.values()[()]
+            sf_x = lo.axis('vpt').edges(overflow='over')
+            sf_y = nlo.values(overflow='over')[()] / lo.values(overflow='over')[()]
 
-        outputrootfile[f'{tag}_{selection}'] = (sf_y,sf_x)
+            outputrootfile[f'{tag}_{selection}_{calc}'] = (sf_y,sf_x)
 
 
 def sf_2d(acc, tag, regex, outputrootfile):
@@ -96,14 +97,20 @@ def sf_2d(acc, tag, regex, outputrootfile):
         os.makedirs(outdir)
 
     # fig, (ax, rax) = plt.subplots(2, 1, figsize=(7,7), gridspec_kw={"height_ratios": (3, 1)}, sharex=True)
+    plt.close('all')
     fig = plt.gcf()
+    fig.clear()
     ax = plt.gca()
     # new_ax = hist.Bin('vpt','LHE V $p_{T}$ (GeV)',list(range(100,500,50)) + list(range(500,1000,100)) + list(range(1000,2000,250)))
     vpt_ax = hist.Bin('vpt','V $p_{T}$ (GeV)',list(range(250,1000,150))+list(range(1000,1200,200))+list(range(1200,200,800)))
+    # vpt_ax = hist.Bin('vpt','V $p_{T}$ (GeV)',list(range(100,800,100))+list(range(800,1200,200))+list(range(1200,2800,800)))
+    # vpt_ax = hist.Bin('vpt','V $p_{T}$ (GeV)',[0,10000])
     mjj_ax = hist.Bin('mjj','M(jj) (GeV)',[200]+list(range(500,2500,500)))
+    # mjj_ax = hist.Bin('mjj','M(jj) (GeV)',[0,1e5])
+
 
     for selection in ['vbf']:
-        dist = f'gen_vpt_{selection}'
+        dist = f'gen_vpt_{selection}_dress'
         h = copy.deepcopy(acc[dist])
 
         h = h.rebin(h.axis('vpt'), vpt_ax)
@@ -140,7 +147,7 @@ def sf_2d(acc, tag, regex, outputrootfile):
                 ax.text(
                         x_centers[ix], 
                         y_centers[iy], 
-                        f'  {sf.T[iy, ix]:.2f} \n$\\pm$ {dsf.T[iy, ix]:.2f}',
+                        f'  {sf.T[iy, ix]:.3f} \n$\\pm$ {dsf.T[iy, ix]:.2f}',
                         ha='center',
                         va='center',
                         color=textcol,
@@ -174,18 +181,24 @@ def sf_2d(acc, tag, regex, outputrootfile):
         # sf_x = lo.axis('vpt').edges()
         # sf_y = nlo.values()[()] / lo.values()[()]
 
-        # outputrootfile[f'{tag}_{selection}'] = (sf_y,sf_x)
+        tup = (sf, xaxis.edges(overflow='over'),yaxis.edges(overflow='over'))
+        print(tup[0].shape)
+        print(tup[1].shape)
+        print(tup[2].shape)
+        outputrootfile[f'2d_{tag}_{selection}'] =  tup
 
 def pdfwgt_sf(vpt):
     return 1/(1.157 + 2.291e-4 * vpt + 6.0612e-7 * vpt**2)
 
 def main():
-    acc = acc_from_dir("./input/2019-10-07_das_lhevpt_v6")
-    # outputrootfile = uproot.recreate(f'2017_gen_v_pt_stat1_qcd_sf.root')
-    # sf_1d(acc, tag='wjet', regex='W.*',outputrootfile=outputrootfile)
-    # sf_1d(acc, tag='dy', regex='.*DY.*',outputrootfile=outputrootfile)
+    acc = acc_from_dir("./input/2019-10-07_das_lhevpt_dressed_v1")
 
-    sf_2d(acc, tag='wjet', regex='W.*',outputrootfile=None)
+    outputrootfile = uproot.recreate(f'2017_gen_v_pt_qcd_sf.root')
+    sf_1d(acc, tag='wjet', regex='W.*',outputrootfile=outputrootfile)
+    sf_1d(acc, tag='dy', regex='.*DY.*',outputrootfile=outputrootfile)
+    # outputrootfile = uproot.recreate(f'test.root')
+    sf_2d(acc, tag='wjet', regex='W.*',outputrootfile=outputrootfile)
+    sf_2d(acc, tag='dy', regex='.*DY.*',outputrootfile=outputrootfile)
 
 
 
