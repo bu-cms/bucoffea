dff9746f4a626c5245a612849438d8a27123d035
Andreas Albert <andreas.albert@cern.ch>
2019-11-05 17:31:14 -0500

    VBF: Apply 2D NLO QCD SF for W, DY.

--- bucoffea/monojet/definitions.py
+++ bucoffea/monojet/definitions.py
@@ -563,11 +563,11 @@ def theory_weights_monojet(weights, df, evaluator, gen_v_pt):
 
 def theory_weights_vbf(weights, df, evaluator, gen_v_pt, mjj):
     if df['is_lo_w']:
-        weights.add("theory", evaluator["qcd_nlo_w_2017"](gen_v_pt) * evaluator["qcd_nnlo_w"](gen_v_pt) * evaluator["ewk_nlo_w"](gen_v_pt))
+        weights.add("theory", evaluator["qcd_nlo_w_2017_2d"](mjj, gen_v_pt) * evaluator["qcd_nnlo_w"](gen_v_pt) * evaluator["ewk_nlo_w"](gen_v_pt))
     elif df['is_lo_w_ewk']:
         weights.add("theory", evaluator["qcd_nlo_w_ewk"](gen_v_pt, mjj))
     elif df['is_lo_z']:
-        weights.add("theory", evaluator["qcd_nlo_z_2017"](gen_v_pt) * evaluator["qcd_nnlo_z"](gen_v_pt) * evaluator["ewk_nlo_z"](gen_v_pt))
+        weights.add("theory", evaluator["qcd_nlo_z_2017_2d"](mjj, gen_v_pt) * evaluator["qcd_nnlo_z"](gen_v_pt) * evaluator["ewk_nlo_z"](gen_v_pt))
     elif df['is_lo_z_ewk']:
         weights.add("theory", evaluator["qcd_nlo_z_ewk"](gen_v_pt, mjj))
     elif df['is_nlo_w']:
