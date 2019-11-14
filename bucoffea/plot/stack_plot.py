ab8c5c1ecd17773411ed9f7b65e1cf64c94a518a
Alp <aakpinar@bu.edu>
2019-11-14 02:40:00 +0100

    Fix axis name for dilepton pt

--- bucoffea/plot/stack_plot.py
+++ bucoffea/plot/stack_plot.py
@@ -83,8 +83,8 @@ class Style():
                     'muon_pt0' : hist.Bin('pt',r'Leading muon $p_{T}$ (GeV)',list(range(0,600,20))),
                     'muon_pt1' : hist.Bin('pt',r'Trailing muon $p_{T}$ (GeV)',list(range(0,600,20))),
                     'muon_pt' : hist.Bin('pt',r'All muon $p_{T}$ (GeV)',list(range(0,600,20))),
-                    'dielectron_pt' : hist.Bin('dilepton_pt',r'Dielectron $p_{T} (GeV)',list(range(0,400,80)) + list(range(400,800,100)) + list(range(800,1100,300))),
-                    'dimuon_pt' : hist.Bin('dilepton_pt',r'Dimuon $p_{T} (GeV)',list(range(0,400,80)) + list(range(400,800,100)) + list(range(800,1100,300))),
+                    'dielectron_pt' : hist.Bin('pt',r'Dielectron $p_{T} (GeV)',list(range(0,400,25)) + list(range(400,800,50)) + list(range(800,1100,100))),
+                    'dimuon_pt' : hist.Bin('pt',r'Dimuon $p_{T} (GeV)',list(range(0,400,25)) + list(range(400,800,50)) + list(range(800,1100,100))),
                     'mjj' : hist.Bin('mjj', r'$M_{jj}$ (GeV)', list(range(200,800,300)) + list(range(800,2000,400)) + [2000, 2750, 3500])
                     }
         }
