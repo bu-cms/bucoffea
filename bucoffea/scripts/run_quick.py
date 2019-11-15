b02d4f8815275017bab6cd665590e26cb6515c84
Alp Akpinar <aakpinar@bu.edu>
2019-11-15 16:45:14 -0600

    Update test dataset to 27Oct19

--- bucoffea/scripts/run_quick.py
+++ bucoffea/scripts/run_quick.py
@@ -19,7 +19,7 @@ def main():
 
     fileset = {
         "VBF_HToInvisible_M125_pow_pythia8_2017" : [
-            "root://cmsxrootd.fnal.gov//store/user/aandreas/nanopost/21Aug19/VBF_HToInvisible_M125_13TeV_TuneCP5_powheg_pythia8/VBF_HToInvisible_M125_pow_pythia8_2017/190822_134734/0000/tree_1.root"
+            "root://cmsxrootd.fnal.gov//store/user/aandreas/nanopost/27Oct19/VBF_HToInvisible_M125_13TeV_TuneCP5_powheg_pythia8/VBF_HToInvisible_M125_pow_pythia8_2017/191027_233758/0000/tree_1.root"
         ]
     }
 
