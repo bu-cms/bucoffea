a63314bb60c8dc048b1dfd528f9a028ba15ddcba
Andreas Albert <andreas.albert@cern.ch>
2019-11-17 16:12:13 -0600

    xroot_format: Deal correctly with xrootd input paths

--- bucoffea/helpers/paths.py
+++ bucoffea/helpers/paths.py
@@ -14,7 +14,7 @@ def xrootd_format(fpath):
     """Ensure that the file path is file:/* or xrootd"""
     if fpath.startswith("/store/"):
         return f"root://cms-xrd-global.cern.ch//{fpath}"
-    elif fpath.startswith("file:"):
+    elif fpath.startswith("file:") or fpath.startswith("root:"):
         return fpath
     else:
         return f"file://{fpath}"
