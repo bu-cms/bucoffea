a50ed388ccf5b7b73e0a2d2fa33ea2dff401b10a
Andreas Albert <andreas.albert@cern.ch>
2020-01-21 17:12:59 -0500

    Fixup

--- bucoffea/monojet/definitions.py
+++ bucoffea/monojet/definitions.py
@@ -614,7 +614,7 @@ def theory_weights_monojet(weights, df, evaluator, gen_v_pt):
             qcd_nlo = evaluator["qcd_nlo_z_2016"](gen_v_pt)
         else:
             qcd_nlo = fitfun(gen_v_pt, 1.423, 2.257e-3, 0.451)
-        theory_weights =  * evaluator["qcd_nnlo_z"](gen_v_pt) * evaluator["ewk_nlo_z"](gen_v_pt)
+        theory_weights =  qcd_nlo * evaluator["qcd_nnlo_z"](gen_v_pt) * evaluator["ewk_nlo_z"](gen_v_pt)
     elif df['is_nlo_w']:
         theory_weights = evaluator["qcd_nnlo_w"](gen_v_pt) * evaluator["ewk_nlo_w"](gen_v_pt)
     elif df['is_nlo_z']:
