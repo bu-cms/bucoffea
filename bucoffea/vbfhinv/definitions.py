6710f505d1c63c4e12a6c0c7d9a38e75072ef118
Alp Akpinar <aakpinar@bu.edu>
2020-01-25 15:39:27 -0500

    Add additional regions for each JES/JER variation

--- bucoffea/vbfhinv/definitions.py
+++ bucoffea/vbfhinv/definitions.py
@@ -197,79 +197,81 @@ def vbfhinv_accumulator(cfg):
 
     return  processor.dict_accumulator(items)
 
-def vbfhinv_regions(cfg):
-    common_cuts = [
-        'veto_ele',
-        'veto_muo',
-        'filt_met',
-        'hemveto',
-        'mindphijr',
-        'recoil',
-        'two_jets',
-        'leadak4_pt_eta',
-        'leadak4_id',
-        'trailak4_pt_eta',
-        'trailak4_id',
-        'hemisphere',
-        'mjj',
-        'dphijj',
-        'detajj',
-        'veto_photon',
-        'veto_tau',
-        'veto_b',
-        'dpfcalo'
-    ]
-
+def vbfhinv_regions(cfg, variations):
     regions = {}
     regions['inclusive'] = ['inclusive']
-
-    # Signal regions (v = mono-V, j = mono-jet)
-    regions['sr_vbf'] = ['trig_met'] + common_cuts
-
-    # For sync mode
-    if cfg.RUN.SYNC:
-        regions['cr_sync'] = [
-            'trig_met',
+    
+    for var in variations:
+        common_cuts = [
+            'veto_ele',
+            'veto_muo',
+            'filt_met',
+            'hemveto',
             'veto_photon',
-            'mindphijr',
-            'recoil',
-            'two_jets',
-            'leadak4_pt_eta',
-            'leadak4_id',
-            'trailak4_pt_eta',
-            'trailak4_id',
-            'hemisphere',
-            'mjj',
-            'dphijj',
-            'detajj'
+            'veto_tau',
+            'veto_b',
+            f'mindphijr{var}',
+            f'recoil{var}',
+            f'two_jets{var}',
+            f'leadak4_pt_eta{var}',
+            f'leadak4_id{var}',
+            f'trailak4_pt_eta{var}',
+            f'trailak4_id{var}',
+            f'hemisphere{var}',
+            f'mjj{var}',
+            f'dphijj{var}',
+            f'detajj{var}'
         ]
 
-    # Dimuon CR
-    cr_2m_cuts = ['trig_met','two_muons', 'at_least_one_tight_mu', 'dimuon_mass', 'veto_ele', 'dimuon_charge'] + common_cuts[1:]
-    cr_2m_cuts.remove('veto_muo')
+        # Signal regions (v = mono-V, j = mono-jet)
+        regions[f'sr_vbf{var}'] = ['trig_met'] + common_cuts
 
-    regions['cr_2m_vbf'] = cr_2m_cuts
+        # For sync mode
+        if cfg.RUN.SYNC:
+            regions['cr_sync'] = [
+                'trig_met',
+                'veto_photon',
+                'mindphijr',
+                'recoil',
+                'two_jets',
+                'leadak4_pt_eta',
+                'leadak4_id',
+                'trailak4_pt_eta',
+                'trailak4_id',
+                'hemisphere',
+                'mjj',
+                'dphijj',
+                'detajj'
+            ]
 
-    # Single muon CR
-    cr_1m_cuts = ['trig_met','one_muon', 'at_least_one_tight_mu',  'veto_ele', 'mt_mu'] + common_cuts[1:]
-    cr_1m_cuts.remove('veto_muo')
-    regions['cr_1m_vbf'] = cr_1m_cuts 
+        # Dimuon CR
+        cr_2m_cuts = ['trig_met','two_muons', 'at_least_one_tight_mu', 'dimuon_mass', 'veto_ele', 'dimuon_charge'] + common_cuts[1:]
+        cr_2m_cuts.remove('veto_muo')
 
-    # Dielectron CR
-    cr_2e_cuts = ['trig_ele','two_electrons', 'at_least_one_tight_el', 'dielectron_mass', 'veto_muo', 'dielectron_charge'] + common_cuts[2:]
-    # cr_2e_cuts.remove('veto_ele')
-    regions['cr_2e_vbf'] = cr_2e_cuts 
+        regions[f'cr_2m_vbf{var}'] = cr_2m_cuts
 
-    # Single electron CR
-    cr_1e_cuts = ['trig_ele','one_electron', 'at_least_one_tight_el', 'veto_muo','met_el','mt_el'] + common_cuts[1:]
-    # cr_1e_cuts.remove('veto_ele')
-    regions['cr_1e_vbf'] =  cr_1e_cuts
+        # Single muon CR
+        cr_1m_cuts = ['trig_met','one_muon', 'at_least_one_tight_mu',  'veto_ele'] + common_cuts[1:]
+        cr_1m_cuts.remove('veto_muo')
+        regions[f'cr_1m_vbf{var}'] = cr_1m_cuts 
 
-    # Photon CR
-    cr_g_cuts = ['trig_photon', 'one_photon', 'at_least_one_tight_photon','photon_pt'] + common_cuts
-    cr_g_cuts.remove('veto_photon')
+        # Dielectron CR
+        cr_2e_cuts = ['trig_ele','two_electrons', 'at_least_one_tight_el', 'dielectron_mass', 'veto_muo', 'dielectron_charge'] + common_cuts[2:]
+        # cr_2e_cuts.remove('veto_ele')
+        regions[f'cr_2e_vbf{var}'] = cr_2e_cuts 
 
-    regions['cr_g_vbf'] = cr_g_cuts
+        # Single electron CR
+        cr_1e_cuts = ['trig_ele','one_electron', 'at_least_one_tight_el', 'veto_muo','met_el'] + common_cuts[1:]
+        # cr_1e_cuts.remove('veto_ele')
+        regions[f'cr_1e_vbf{var}'] =  cr_1e_cuts
+
+        # Photon CR
+        cr_g_cuts = ['trig_photon', 'one_photon', 'at_least_one_tight_photon','photon_pt'] + common_cuts
+        cr_g_cuts.remove('veto_photon')
+
+        regions[f'cr_g_vbf{var}'] = cr_g_cuts
+
+    ##########################################
 
     if cfg.RUN.SYNC:
         regions['sync_sr_vbf_round1'] = [
