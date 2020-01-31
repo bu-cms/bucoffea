387a785352f173932413378b07c245fe1d4e2069
Andreas Albert <andreas.albert@cern.ch>
2020-01-31 16:26:13 -0500

    Implement electron trigger SF

--- bucoffea/monojet/monojetProcessor.py
+++ bucoffea/monojet/monojetProcessor.py
@@ -362,7 +362,7 @@ class monojetProcessor(processor.ProcessorABC):
             region_weights = copy.deepcopy(weights)
             if not df['is_data']:
                 if re.match(r'cr_(\d+)e.*', region):
-                    region_weights.add('trigger', np.ones(df.size))
+                    region_weights.add('trigger', evaluator["trigger_electron"](electrons[leadelectron_index].eta,electrons[leadelectron_index].pt))
                 elif re.match(r'cr_(\d+)m.*', region) or re.match('sr_.*', region):
                     region_weights.add('trigger', evaluator["trigger_met"](df['recoil_pt']))
                 elif re.match(r'cr_g.*', region):
