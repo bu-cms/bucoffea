29342fc07c83bf6c05f5e6ede87aad0c349fbb57
Andreas Albert <andreas.albert@cern.ch>
2020-02-04 22:55:04 +0100

    Update event_in_file

--- bucoffea/data/sync/event_in_file.py
+++ bucoffea/data/sync/event_in_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-
+import numpy as np
 import sys
 import argparse
 import uproot
+import tqdm
 def commandline():
     parser = argparse.ArgumentParser(prog='Check which file a given event is in.')
     parser.add_argument('files', type=str, help='Input files to use.',nargs='+')
@@ -13,11 +14,11 @@ def commandline():
 def main():
     args =commandline()
 
-    for path in args.files:
+    for path in tqdm.tqdm(args.files):
         f = uproot.open(path)
 
         events = f['Events'].array('event')
-        if args.event in events:
+        if np.any(events==args.event):
             print(f'Found {args.event} {path}')
             break
         # if any(args.event == ):
