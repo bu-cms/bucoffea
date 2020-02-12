f542d728343c33c525976cfb6442453eecf9179d
Andreas Albert <andreas.albert@cern.ch>
2020-02-12 13:48:26 -0500

    Stitch plot

--- bucoffea/plot/studies/stitching/plot_stitching.py
+++ bucoffea/plot/studies/stitching/plot_stitching.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 from coffea import hist
 
 from bucoffea.plot.util import (acc_from_dir, merge_datasets, merge_extensions,
-                                scale_xs_lumi)
+                                scale_xs_lumi,klepto_load)
 
 pjoin = os.path.join
 def plot_ht_stitching(acc, tag, regex):
@@ -22,7 +22,7 @@ def plot_ht_stitching(acc, tag, regex):
         scale_xs_lumi(h)
         h = merge_datasets(h)
 
-        fig, ax, _ = hist.plot1d(
+        ax = hist.plot1d(
             h[re.compile(regex)],
             overlay='dataset',
             overflow='all',
@@ -30,17 +30,24 @@ def plot_ht_stitching(acc, tag, regex):
         plt.yscale('log')
         plt.ylim(1e-3,1e6)
 
-        fig.savefig(pjoin(outdir,f'{tag}_{dist}.pdf'))
+        for ext in ['pdf','png']:
+            ax.figure.savefig(pjoin(outdir,f'{tag}_{dist}.{ext}'))
+        ax.figure.clf()
 def main():
-    acc = acc_from_dir("./input/")
-    # plot_ht_stitching(acc, tag='dy', regex='DY.*HT(?!(.*new.*))')
-    # acc = acc_from_dir("./input/wjet")
-    plot_ht_stitching(acc, tag='wjet', regex='W.*HT(?!(.*new.*))')
-    # acc = acc_from_dir("./input/znunu")
-    plot_ht_stitching(acc, tag='znunu', regex='ZJets.*NuNu.*HT(?!(.*new.*))')
-    # acc = acc_from_dir("./input/gjets")
-    plot_ht_stitching(acc, tag='gjets', regex='GJets.*HT(?!(.*new.*))')
-    plot_ht_stitching(acc, tag='qcd', regex='QCD.*HT(?!(.*new.*))')
+    # acc = acc_from_dir("./input/")
+
+    acc = klepto_load('../lo_vs_nlo/input/2020-01-21_correction_facs_updated')
+    acc.load('nevents')
+    acc.load("sumw")
+    acc.load("sumw_pileup")
+    acc.load('lhe_ht')
+    plot_ht_stitching(acc, tag='wjet', regex='WJet.*LNu.*HT.*')
+    plot_ht_stitching(acc, tag='dy', regex='DYJetsToLL.*HT.*')
+    # # acc = acc_from_dir("./input/znunu")
+    # plot_ht_stitching(acc, tag='znunu', regex='ZJets.*NuNu.*HT.*')
+    # # acc = acc_from_dir("./input/gjets")
+    # plot_ht_stitching(acc, tag='gjets', regex='GJets_HT.*')
+    # plot_ht_stitching(acc, tag='qcd', regex='QCD.*HT.*')
 
 
 
