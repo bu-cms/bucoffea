e38f2674b46b4f33f5e5810a03656a5c81e95afd
Alp Akpinar <aakpinar@bu.edu>
2020-05-26 01:48:22 -0500

    Use 23May20 tag for input, which contains datasets with full JME fix

--- bucoffea/execute/dataset_definitions.py
+++ bucoffea/execute/dataset_definitions.py
@@ -199,7 +199,7 @@ def files_from_eos(regex):
                 fileset[key] = fileset_16jul[key]
     elif 'lpc' in host:
         topdir = '/eos/uscms/store/user/aakpinar/nanopost/'
-        tag = '15May20'
+        tag = '23May20'
         fileset = find_files_eos(pjoin(topdir, tag), regex)
 
     return fileset
