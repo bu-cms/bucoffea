49592c8ac60e4c8c39210394ea976e57766d5d00
Alp Akpinar <aakpinar@bu.edu>
2020-06-07 13:16:32 -0500

    Update the dataset tag: Use the datasets with JES fix

--- bucoffea/execute/dataset_definitions.py
+++ bucoffea/execute/dataset_definitions.py
@@ -198,8 +198,8 @@ def files_from_eos(regex):
             else:
                 fileset[key] = fileset_16jul[key]
     elif 'lpc' in host:
-        topdir = '/eos/uscms/store/user/aakpinar/nanopost/'
-        tag = '23May20'
+        topdir = '/eos/uscms/store/user/aalbert/nanopost/'
+        tag = '05Jun20v5'
         fileset = find_files_eos(pjoin(topdir, tag), regex)
 
     return fileset
