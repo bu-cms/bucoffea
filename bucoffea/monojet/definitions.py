3a8100f90ebc06a40f46dbaa79bbddf9dd59444a
Andreas Albert <andreas.albert@cern.ch>
2020-07-17 11:13:50 -0500

    Propagate JER change to PU ID

--- bucoffea/monojet/definitions.py
+++ bucoffea/monojet/definitions.py
@@ -383,7 +383,7 @@ def setup_candidates(df, cfg):
         mass=np.zeros_like(df['Jet_pt']),
         looseId=(df['Jet_jetId']&2) == 2, # bitmask: 1 = loose, 2 = tight, 3 = tight + lep veto
         tightId=(df['Jet_jetId']&2) == 2, # bitmask: 1 = loose, 2 = tight, 3 = tight + lep veto
-        puid=((df['Jet_puId']&2>0) | (df[f'Jet_pt{jes_suffix}']>50)), # medium pileup jet ID
+        puid=((df['Jet_puId']&2>0) | ((df[f'Jet_pt{jes_suffix}'] if (df['is_data'] or cfg.AK4.JER) else df[f'Jet_pt{jes_suffix}']/df['Jet_corr_JER'])>50)), # medium pileup jet ID
         csvv2=df["Jet_btagCSVV2"],
         deepcsv=df['Jet_btagDeepB'],
         nef=df['Jet_neEmEF'],
