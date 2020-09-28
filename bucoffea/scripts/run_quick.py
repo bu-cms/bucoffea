3ea1c9e69aa95512dd9d7a77760d26362024b46e
Alp Akpinar <aakpinar@bu.edu>
2020-09-27 19:08:21 -0500

    Point to the new 03Sep20v7 files

--- bucoffea/scripts/run_quick.py
+++ bucoffea/scripts/run_quick.py
@@ -19,7 +19,7 @@ def main():
 
     fileset = {
         "VBF_HToInvisible_M125_pow_pythia8_2017" : [
-            "root://cmsxrootd.fnal.gov//store/user/aandreas/nanopost/05Jun20v5/VBF_HToInvisible_M125_13TeV_TuneCP5_powheg_pythia8/VBF_HToInvisible_M125_pow_pythia8_2017/200605_233117/0000/tree_1.root"
+            "root://cmsxrootd.fnal.gov//store/user/aandreas/nanopost/03Sep20v7/VBF_HToInvisible_M125_13TeV_TuneCP5_powheg_pythia8/VBF_HToInvisible_M125_pow_pythia8_2017/200925_184136/0000/tree_1.root"
         ]
     }
 
