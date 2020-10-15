ab9d04c968c4ce2a39de6d24114a25ff98ac5204
Siqi Yuan <yuansiqi0114@hotmail.com>
2020-10-14 21:26:27 -0400

    add deepAK8 W tagger SF for medium WP (2.5%)

--- bucoffea/plot/studies/wtag_sf/makeSF.py
+++ bucoffea/plot/studies/wtag_sf/makeSF.py
@@ -6,7 +6,7 @@ outfilename = "../../../data/sf/ak8/wtag_eff_SF.root"
 outfile = ROOT.TFile.Open(outfilename, "recreate")
 
 #ref: These numbers are read from https://twiki.cern.ch/twiki/bin/viewauth/CMS/DeepAKXTagging#DeepAK8_V2_Working_Points_and_Sc
-#loose: 5%, tight: 0.5%
+#loose: 5%, tight: 0.5%, medium: 2.5%
 binning    = [200, 300, 400, 800]
 SFs = {}
 SFs[(2017,'tight'  )] = [ [1.00 , 0.08        , 0.07],
@@ -21,6 +21,12 @@ SFs[(2017,'loose'  )] = [ [1.05 , 0.04        , 0.04],
 SFs[(2017,'loosemd')] = [ [1.00 , 0.04        , 0.04],
                           [0.94 , 0.03        , 0.03],
                           [0.95 , 0.03        , 0.03],]
+SFs[(2017,'medium'  )] = [[1.19 , 0.04        , 0.04],
+                          [1.03 , 0.11        , 0.11],
+                          [0.95 , 0.08        , 0.08],]
+SFs[(2017,'mediummd')] = [[0.94 , 0.05        , 0.05],
+                          [0.92 , 0.04        , 0.04],
+                          [0.93 , 0.05        , 0.05],]
 SFs[(2018,'tight'  )] = [ [0.87 , 0.06        , 0.06],
                           [0.86 , 0.05        , 0.05],
                           [0.85 , 0.06        , 0.05],]
@@ -33,6 +39,12 @@ SFs[(2018,'loose'  )] = [ [1.04 , 0.02        , 0.02],
 SFs[(2018,'loosemd')] = [ [0.96 , 0.03        , 0.03],
                           [0.93 , 0.02        , 0.03],
                           [0.96 , 0.03        , 0.03],]
+SFs[(2018,'medium'  )] = [[1.10 , 0.09        , 0.07],
+                          [0.96 , 0.07        , 0.08],
+                          [1.04 , 0.10        , 0.12],]
+SFs[(2018,'mediummd')] = [[0.91 , 0.05        , 0.05],
+                          [0.86 , 0.03        , 0.04],
+                          [0.90 , 0.04        , 0.04],]
 
 for year in [2017,2018]:
     for wp in ['loose','loosemd','tight','tightmd']:
