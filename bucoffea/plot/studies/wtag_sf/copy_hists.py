0106dd7be118ce3e011650be77cf5c1f9465f49c
Siqi Yuan <yuansiqi0114@hotmail.com>
2020-10-15 20:03:35 -0400

    update script to copy histograms to data/sf/ak8

--- bucoffea/plot/studies/wtag_sf/copy_hists.py
+++ bucoffea/plot/studies/wtag_sf/copy_hists.py
@@ -3,17 +3,24 @@
 import ROOT
 
 input_filename = 'wtag_mistag_SF.root'
-output_filename = '../../../data/sf/ak8/wtag_mistag_SF.root'
+output_filename = '../../../../data/sf/ak8/wtag_mistag_SF.root'
 
 input_file = ROOT.TFile.Open(input_filename)
 output_file = ROOT.TFile.Open(output_filename, "recreate")
 
+input_file.ls()
+
 for year in [2017,2018]:
     for wp in ['loose','loosemd','tightmd','tight']:
-        hist_name = f'Wmistag_{year}_{wp}_ak8_pt'
-        hist = input_file.Get(hist_name)
-        output_file.cd()
-        hist.Write()
+        for lepton_tag in ['g','wz']:
+            hist_name = f'mistag_SF_{lepton_tag}_{wp}_{year}'
+            hist = input_file.Get(hist_name)
+            output_file.cd()
+            try:
+                hist.Write()
+            except ReferenceError:
+                print(f"histogram {hist_name} not found")
+                pass
 
 input_file.Close()
 output_file.Close()
