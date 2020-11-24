2fa670bbf4c966533fc088fa5b82e272f0722687
Siqi Yuan <yuansiqi0114@hotmail.com>
2020-11-24 16:10:47 -0500

    update mistag scale factors

--- bucoffea/plot/studies/wtag_sf/copy_hists.py
+++ bucoffea/plot/studies/wtag_sf/copy_hists.py
@@ -2,16 +2,15 @@
 
 import ROOT
 
-input_filename = 'wtag_mistag_SF.root'
-output_filename = '../../../../data/sf/ak8/wtag_mistag_SF.root'
+input_filename = 'output_mistag/merged_2020_11_23_03Sep20v7_Wmass65To120/output_bin1_massden_massnum_nlogjet_realVSF1/wtag_mistag_SF.root'
+output_filename = '../../../data/sf/ak8/wtag_mistag_SF.root'
 
 input_file = ROOT.TFile.Open(input_filename)
 output_file = ROOT.TFile.Open(output_filename, "recreate")
 
-input_file.ls()
 
 for year in [2017,2018]:
-    for wp in ['loose','loosemd','tightmd','tight']:
+    for wp in ['loose','tight']:
         for lepton_tag in ['g','wz']:
             hist_name = f'mistag_SF_{lepton_tag}_{wp}_{year}'
             hist = input_file.Get(hist_name)
@@ -24,3 +23,4 @@ for year in [2017,2018]:
 
 input_file.Close()
 output_file.Close()
+print(f"copied histograms from file {input_filename} to {output_filename}")
