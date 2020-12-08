5fd6b56b178926d409c3ee6b96a5f4fbb6c8ef29
Andreas Albert <andreas.albert@cern.ch>
2020-12-08 11:46:10 -0600

    Update monojet w kfac

--- bucoffea/monojet/definitions.py
+++ bucoffea/monojet/definitions.py
@@ -743,7 +743,7 @@ def theory_weights_monojet(weights, df, evaluator, gen_v_pt):
         if extract_year(df['dataset']) == 2016:
             qcd_nlo = evaluator["qcd_nlo_w_2016"](gen_v_pt)
         else:
-            qcd_nlo = fitfun(gen_v_pt, 1.053, 3.163e-3, 0.746)
+            qcd_nlo = evaluator["kfac_wjets_filter"](gen_v_pt)
         theory_weights =  qcd_nlo * evaluator["ewk_nlo_w"](gen_v_pt)
     elif df['is_lo_z']:
         if df['is_lo_znunu']:
