40a4d78bd3e9ca85bdf897fae064d5867e697a13
Alp Akpinar <aakpinar@bu.edu>
2021-02-16 12:51:38 -0600

    Make MET correction configurable

--- bucoffea/vbfhinv/vbfhinvProcessor.py
+++ bucoffea/vbfhinv/vbfhinvProcessor.py
@@ -186,8 +186,9 @@ class vbfhinvProcessor(processor.ProcessorABC):
         # Filtering ak4 jets according to pileup ID
         ak4 = ak4[ak4.puid]
 
-        # Recalculate MET pt and phi based on npv-corrections
-        met_pt, met_phi = recalculate_met_based_on_npv(df, met_pt, met_phi)
+        if cfg.RUN.CORRECT_MET:
+            # Recalculate MET pt and phi based on npv-corrections
+            met_pt, met_phi = recalculate_met_based_on_npv(df, met_pt, met_phi)
 
         # Muons
         df['is_tight_muon'] = muons.tightId \
