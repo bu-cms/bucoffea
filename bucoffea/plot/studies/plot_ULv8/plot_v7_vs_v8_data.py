d827ebd9fe40b1754ebfc337a1a619733c53d6e8
Alp Akpinar <aakpinar@bu.edu>
2021-03-04 17:56:56 -0500

    Plot more distributions

--- bucoffea/plot/studies/plot_ULv8/plot_v7_vs_v8_data.py
+++ bucoffea/plot/studies/plot_ULv8/plot_v7_vs_v8_data.py
@@ -28,6 +28,14 @@ def preprocess(h, acc, distribution, region='sr_vbf', dataset='MET_2017'):
         mjj_ax = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.])
         h = h.rebin('mjj', mjj_ax)
 
+    elif distribution == 'ak4_pt0':
+        pt_ax = hist.Bin('jetpt',r'Leading jet $p_{T}$ (GeV)',list(range(80,800,20)) )
+        h = h.rebin('jetpt', pt_ax)
+
+    elif distribution == 'ak4_pt1':
+        pt_ax = hist.Bin('jetpt',r'Trailing jet $p_{T}$ (GeV)',list(range(40,600,20)) )
+        h = h.rebin('jetpt', pt_ax)
+
     return h
 
 def plot_v7_vs_v8_data(acc_v7, acc_v8, distribution, region='sr_vbf'):
@@ -80,6 +88,8 @@ def plot_v7_vs_v8_data(acc_v7, acc_v8, distribution, region='sr_vbf'):
     newxlabels = {
         'ak4_eta0' : r'Leading jet $\eta$',
         'ak4_eta1' : r'Trailing jet $\eta$',
+        'ak4_central_eta' : r'Central VBF jet $\eta$',
+        'ak4_forward_eta' : r'Forward VBF jet $\eta$',
         'detajj' : r'$\Delta\eta_{jj}$',
     }
 
@@ -89,7 +99,7 @@ def plot_v7_vs_v8_data(acc_v7, acc_v8, distribution, region='sr_vbf'):
 
     rax.set_ylabel('v8 / v7')
     rax.grid(True)
-    if distribution in ['mjj','detajj']:
+    if distribution in ['mjj','detajj','ak4_pt0','ak4_pt1']:
         rax.set_ylim(0.6,1.4)
         loc = MultipleLocator(0.2)
         rax.yaxis.set_major_locator(loc)
@@ -109,8 +119,8 @@ def plot_v7_vs_v8_data(acc_v7, acc_v8, distribution, region='sr_vbf'):
     print(f'File saved: {outpath}')
 
 def main():
-    inpath_v7 = '../../../submission/merged_2021-03-03_vbfhinv_03Sep20v7_MET_2017'
-    inpath_v8 = '../../../submission/merged_2021-03-02_vbfhinv_MET2017_newUL'
+    inpath_v7 = '../../../submission/merged_2021-03-04_vbfhinv_03Sep20v7_MET_2017'
+    inpath_v8 = '../../../submission/merged_2021-03-04_vbfhinv_ULv8_MET_2017'
     acc_v7 = dir_archive(inpath_v7) 
     acc_v8 = dir_archive(inpath_v8) 
 
@@ -118,9 +128,10 @@ def main():
         acc.load('sumw')
         acc.load('sumw2')
 
-    distributions = ['mjj', 'detajj', 'ak4_eta0', 'ak4_eta1']
+    distributions = ['mjj', 'detajj', 'ak4_eta0', 'ak4_eta1', 'ak4_pt0', 'ak4_pt1', 'ak4_central_eta', 'ak4_forward_eta']
 
-    regions = ['sr_vbf', 'sr_vbf_no_mitigationcuts', 'sr_vbf_no_hfhf']
+    # regions = ['sr_vbf', 'sr_vbf_no_mitigationcuts', 'sr_vbf_no_hfhf']
+    regions = ['sr_vbf']
 
     for region in regions:
         for distribution in distributions:
