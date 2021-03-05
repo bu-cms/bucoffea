e071d3774a11263304a84b8643335136f5c9676b
Alp Akpinar <aakpinar@bu.edu>
2021-03-05 15:08:16 -0500

    2D sigma eta vs phi plotter

--- bucoffea/plot/studies/plot_ULv8/plot_zmumu_events.py
+++ bucoffea/plot/studies/plot_ULv8/plot_zmumu_events.py
@@ -4,6 +4,7 @@ import os
 import sys
 import re
 import numpy as np
+import matplotlib.colors as colors
 
 from coffea import hist
 from scipy.stats import distributions
@@ -108,6 +109,46 @@ def plot_zmumu_data_vs_mc(acc, outtag, distribution):
 
         print(f'File saved: {outpath}')
 
+def plot_zmumu_eta_vs_phi(acc, outtag, distribution, region='cr_2m_vbf_no_noisecuts', dtype='data'):
+    '''Plot 2D sigma eta vs. phi distribution for data or MC.'''
+    acc.load(distribution)
+    h = acc[distribution]
+
+    h = merge_extensions(h, acc, reweight_pu=False)
+    scale_xs_lumi(h)
+    h = merge_datasets(h)
+
+    h = h.integrate('dataset', f'MET_2017').integrate('region', region)
+
+    fig, ax = plt.subplots()
+    hist.plot2d(h, ax=ax, xaxis='sigmaetaeta',  patch_opts={'norm': colors.LogNorm(1e-3,1e1)})
+
+    dataset_tag = 'MET' if dtype == 'data' else 'DY'
+
+    ax.text(0., 1., f'{dataset_tag} UL v8 2017',
+        fontsize=14,
+        ha='left',
+        va='bottom',
+        transform=ax.transAxes
+    )
+
+    ax.text(1., 1., r'HF Jets, $p_T > 80 \ GeV$',
+        fontsize=14,
+        ha='right',
+        va='bottom',
+        transform=ax.transAxes
+    )
+
+    # Save figure
+    outdir = f'./output/{outtag}/2d'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    outpath = pjoin(outdir, f'{dtype}_2017_{distribution}.pdf')
+    fig.savefig(outpath)
+    plt.close(fig)
+    print(f'File saved: {outpath}')
+
 def main():
     inpath = sys.argv[1]
     acc = dir_archive(inpath)
@@ -127,6 +168,17 @@ def main():
 
         plot_split_by_eta(acc, outtag, distribution, dataset_regex='DY.*2017')
         plot_split_by_eta(acc, outtag, distribution, dataset_regex='MET.*2017')
+
+    # 2D sigma eta/phi distributions for data and MC
+    distributions_2d = [
+        'ak4_sigma_eta_phi',
+        'ak4_sigma_eta_phi0',
+        'ak4_sigma_eta_phi1',
+    ]
+
+    for distribution in distributions_2d:
+        plot_zmumu_eta_vs_phi(acc, outtag, distribution, dtype='data')
+        plot_zmumu_eta_vs_phi(acc, outtag, distribution, dtype='mc')
 
 if __name__ == '__main__':
     main()
