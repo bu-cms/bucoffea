40d69aea3e5e778c627f574db778e42a16f804d7
Alp Akpinar <aakpinar@bu.edu>
2021-03-24 20:18:47 -0400

    Plot MC as well

--- bucoffea/plot/studies/plot_ULv8/plot_leading_jet_eta_vs_pt.py
+++ bucoffea/plot/studies/plot_ULv8/plot_leading_jet_eta_vs_pt.py
@@ -14,7 +14,7 @@ from pprint import pprint
 
 pjoin = os.path.join
 
-def plot_leading_jet_eta_vs_pt(acc, outtag, region='cr_2m_vbf_relaxed_sel'):
+def plot_leading_jet_eta_vs_pt(acc, outtag, region='cr_2m_vbf_relaxed_sel', dataset_tag='MET_2017'):
     distribution = 'ak4_pt0_eta0'
     acc.load(distribution)
     h = acc[distribution]
@@ -23,7 +23,12 @@ def plot_leading_jet_eta_vs_pt(acc, outtag, region='cr_2m_vbf_relaxed_sel'):
     scale_xs_lumi(h)
     h = merge_datasets(h)
 
-    h = h.integrate('region', region).integrate('dataset', 'MET_2017')
+    datasetregex = {
+        'MET_2017' : 'MET_2017',
+        'DY_2017' : re.compile('DYJetsToLL.*2017'),
+    }
+
+    h = h.integrate('region', region).integrate('dataset', datasetregex[dataset_tag])
 
     pt_ax = hist.Bin('jetpt', r'Leading Jet $p_T \ (GeV)$', list(range(80,780,50)))
     h = h.rebin('jetpt', pt_ax)
@@ -32,7 +37,7 @@ def plot_leading_jet_eta_vs_pt(acc, outtag, region='cr_2m_vbf_relaxed_sel'):
     hist.plot2d(h, ax=ax, xaxis='jeteta',patch_opts={'norm' : colors.LogNorm(1e-1,1e3)})
     ax.set_xlabel(r'Leading Jet $\eta$')
 
-    ax.text(0.,1.,'MET 2017',
+    ax.text(0.,1.,dataset_tag.replace('_', ' '),
         fontsize=14,
         ha='left',
         va='bottom',
@@ -49,7 +54,8 @@ def plot_leading_jet_eta_vs_pt(acc, outtag, region='cr_2m_vbf_relaxed_sel'):
     outdir = f'./output/{outtag}'
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    outpath = pjoin(outdir, f'{distribution}.pdf')
+
+    outpath = pjoin(outdir, f'{dataset_tag}_{distribution}.pdf')
     fig.savefig(outpath)
     plt.close(fig)
 
@@ -63,7 +69,9 @@ def main():
     
     outtag = re.findall('merged_.*', inpath)[0].replace('/','')
 
-    plot_leading_jet_eta_vs_pt(acc, outtag)
+    dataset_tag = 'DY_2017' if 'DY' in outtag else 'MET_2017'
+
+    plot_leading_jet_eta_vs_pt(acc, outtag, dataset_tag=dataset_tag)
 
 if __name__ == '__main__':
     main()
