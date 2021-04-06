a62ee82f0fc35f25710f0d860553ccbbb4e93d31
Alp Akpinar <aakpinar@bu.edu>
2021-04-05 20:33:38 -0400

    Plot total MC bkg in SR

--- bucoffea/plot/studies/2021-03-27_run_ULv8_data/plot_mc_in_sr.py
+++ bucoffea/plot/studies/2021-03-27_run_ULv8_data/plot_mc_in_sr.py
@@ -13,7 +13,8 @@ from klepto.archives import dir_archive
 
 pjoin = os.path.join
 
-def plot_data_in_qcd_cr(acc, outtag, dataset='MET_2017', distribution='mjj', region='sr_vbf_fail_hf_cuts'):
+def plot_mc_in_sr(acc, outtag, region='sr_vbf', distribution='mjj'):
+    '''Plot total MC background in SR'''
     acc.load(distribution)
     h = acc[distribution]
 
@@ -25,22 +26,23 @@ def plot_data_in_qcd_cr(acc, outtag, dataset='MET_2017', distribution='mjj', reg
         mjj_ax = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.])
         h = h.rebin('mjj', mjj_ax)
 
+    dataset = re.compile('(ZJetsToNuNu.*|EW.*|Top_FXFX.*|Diboson.*|.*DYJetsToLL_M-50_HT_MLM.*|.*WJetsToLNu.*HT.*).*2017')
     h = h.integrate('region', region).integrate('dataset', dataset)
 
     fig, ax = plt.subplots()
     hist.plot1d(h, ax=ax)
-    
-    ax.set_yscale('log')
-    ax.set_ylim(1e0,1e4)
 
-    ax.text(0.,1.,'UL v8 MET 2017',
+    ax.set_yscale('log')
+    ax.set_ylim(1e0,1e6)
+
+    ax.text(0.,1.,'Total MC Bkg',
         fontsize=14,
         ha='left',
         va='bottom',
         transform=ax.transAxes
     )
 
-    ax.text(1.,1.,'QCD CR',
+    ax.text(1.,1.,'VBF SR',
         fontsize=14,
         ha='right',
         va='bottom',
@@ -48,12 +50,11 @@ def plot_data_in_qcd_cr(acc, outtag, dataset='MET_2017', distribution='mjj', reg
     )
 
     ax.get_legend().remove()
-    ax.yaxis.set_ticks_position('both')
 
     outdir = f'./output/{outtag}'
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    outpath = pjoin(outdir, f'data_in_qcd_cr_{distribution}.pdf')
+    outpath = pjoin(outdir, f'mc_in_sr_{distribution}.pdf')
     fig.savefig(outpath)
     plt.close(fig)
 
@@ -65,7 +66,7 @@ def main():
 
     outtag = re.findall('merged_.*', inpath)[0].replace('/','')
 
-    plot_data_in_qcd_cr(acc, outtag)
+    plot_mc_in_sr(acc, outtag)
 
 if __name__ == '__main__':
     main()
