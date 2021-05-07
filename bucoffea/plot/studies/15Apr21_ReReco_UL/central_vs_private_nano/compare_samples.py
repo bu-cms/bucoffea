bf6b1ae544d5cd7c3e4ad04b259723ad90e01393
Alp Akpinar <aakpinar@bu.edu>
2021-05-07 12:52:09 -0400

    Update central/private comparison script

--- bucoffea/plot/studies/15Apr21_ReReco_UL/central_vs_private_nano/compare_samples.py
+++ bucoffea/plot/studies/15Apr21_ReReco_UL/central_vs_private_nano/compare_samples.py
@@ -2,6 +2,7 @@
 import os
 import re
 import sys
+import warnings
 import numpy as np
 import mplhep as hep
 
@@ -16,17 +17,27 @@ from pprint import pprint
 
 pjoin = os.path.join
 
+warnings.filterwarnings('ignore')
+
+BINNINGS = {
+    'mjj' : hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500.]),
+    'ak4_pt0' : hist.Bin('jetpt',r'Leading AK4 jet $p_{T}$ (GeV)',list(range(80,600,20)) + list(range(600,1000,20)) ),
+    'ak4_pt1' : hist.Bin('jetpt',r'Trailing AK4 jet $p_{T}$ (GeV)',list(range(40,600,20)) + list(range(600,1000,20)) ),
+}
+
 def preprocess(h, acc, region, distribution='mjj'):
     h = merge_extensions(h, acc)
     scale_xs_lumi(h)
     h = merge_datasets(h)
     
-    if distribution == 'mjj':
-        mjj_ax = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500.])
-        h = h.rebin('mjj', mjj_ax)
+    # Rebin if necessary
+    if distribution in BINNINGS.keys():
+        new_ax = BINNINGS[distribution]
+        h = h.rebin(new_ax.name, new_ax)
 
     h = h.integrate('region', region)
-    return h    
+    
+    return h
 
 def compare_samples(acc_dict, region, distribution='mjj', years=[2017]):
     '''Compare samples from private and central Nano production.'''
@@ -76,6 +87,15 @@ def compare_samples(acc_dict, region, distribution='mjj', years=[2017]):
                 error_opts=data_err_opts
             )
 
+            new_xlabels = {
+                'ak4_eta0' : r'Leading Jet $\eta$',
+                'ak4_eta1' : r'Trailing Jet $\eta$',
+            }
+
+            if distribution in new_xlabels.keys():
+                ax.set_xlabel(new_xlabels[distribution])
+                rax.set_xlabel(new_xlabels[distribution])
+
             rax.grid(True)
             rax.set_ylim(0.5,1.5)
             rax.set_ylabel('Private / Central')
@@ -83,15 +103,15 @@ def compare_samples(acc_dict, region, distribution='mjj', years=[2017]):
             outdir = './output'
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
-            outpath = pjoin(outdir, f'private_vs_central_{tag}_{year}.pdf')
+            outpath = pjoin(outdir, f'private_vs_central_{tag}_{distribution}_{year}.pdf')
             fig.savefig(outpath)
             plt.close(fig)
             print(f'File saved: {outpath}')
 
 def main():
     acc_dict = {
-        'private' : dir_archive( bucoffea_path('submission/') ),
-        'central' : dir_archive( bucoffea_path('submission/') ),
+        'private' : dir_archive( bucoffea_path('submission/merged_2021-05-03_vbfhinv_ULv8_05Feb21_trigSF_update') ),
+        'central' : dir_archive( bucoffea_path('submission/merged_2021-05-07_vbfhinv_ULv8_05Feb21_central_EWK_V') ),
     }
     
     for acc in acc_dict.values():
