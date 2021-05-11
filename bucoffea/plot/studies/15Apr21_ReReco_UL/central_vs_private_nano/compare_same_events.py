c66093e184de45d41b542d33224fab0e568d9f30
Alp Akpinar <aakpinar@bu.edu>
2021-05-11 16:29:10 -0400

    Update central/private prod comparison

--- bucoffea/plot/studies/15Apr21_ReReco_UL/central_vs_private_nano/compare_same_events.py
+++ bucoffea/plot/studies/15Apr21_ReReco_UL/central_vs_private_nano/compare_same_events.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from bucoffea.plot.util import fig_ratio
 import os
 import sys
 import re
@@ -9,6 +8,7 @@ import numpy as np
 import pandas as pd
 import mplhep as hep
 
+from bucoffea.plot.util import fig_ratio
 from coffea.hist import poisson_interval
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator
@@ -24,6 +24,26 @@ LABELS = {
     'ewk_wminusev' : r'EWK $W^{-}(e\nu)$',
     'ewk_wplusmv' : r'EWK $W^{+}(\mu\nu)$',
     'ewk_wplusev' : r'EWK $W^{+}(e\nu)$',
+}
+
+BINNINGS = {
+    'mjj' : [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.],
+    'leadak4_ptnano' : list(range(80,1000,20)),
+    'leadak4_eta' : np.linspace(-5,5),
+    'trailak4_ptnano' : list(range(40,800,20)),
+    'trailak4_eta' : np.linspace(-5,5),
+    'recoil_pt_nano' : [ 250,  280,  310,  340,  370,  400,  430,  470,  510, 550,  590,  640,  690,  740,  790,  840,  900,  960, 1020, 1090, 1160, 1250, 1400],
+    'recoil_phi_nano' : np.linspace(-np.pi, np.pi)
+}
+
+XLABELS = {
+    'mjj' : r'$M_{jj} \ (GeV)$',
+    'leadak4_ptnano' : r'Leading Jet $p_T \ (GeV)$',
+    'leadak4_eta' : r'Leading Jet $\eta$',
+    'trailak4_ptnano' : r'Trailing Jet $p_T \ (GeV)$',
+    'trailak4_eta' : r'Trailing Jet $\eta$',
+    'recoil_pt_nano' : r'Recoil $p_T \ (GeV)$',
+    'recoil_phi_nano' : r'Recoil $\phi$',
 }
 
 XSECTIONS = {
@@ -57,6 +77,47 @@ SUMW2 = {
     'ewk_zvv' : 2985000.0,
 }
 
+def event_by_event_comparison(path_central, path_private, region, tag, distribution='mjj', year=2017):
+    df_central = uproot.open(path_central)[region].pandas.df()
+    df_private = uproot.open(path_private)[region].pandas.df()
+
+    # Determine sample year
+    year = re.findall('201\d', path_central)[0]
+
+    # Merge dataframes based on run,event,lumi
+    merged_df = df_central.merge(df_private, on=['run', 'event', 'lumi'], suffixes=['_central', '_private'])
+
+    central_vals = merged_df[f'{distribution}_central']
+    private_vals = merged_df[f'{distribution}_private']
+
+    diff = (central_vals - private_vals) / (central_vals + private_vals)
+    fig, ax = plt.subplots()
+    bins= np.linspace(-0.05,0.05)
+    ax.hist(diff, bins=bins)
+    
+    ax.text(0.,1.,LABELS[tag],
+        fontsize=14,
+        ha='left',
+        va='bottom',
+        transform=ax.transAxes
+    )
+
+    ax.text(1.,1.,year,
+        fontsize=14,
+        ha='right',
+        va='bottom',
+        transform=ax.transAxes
+    )
+
+    outdir = './output/11May21/with_event_match/deltas'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    outpath = pjoin(outdir, f'{tag}_{distribution}_{year}.pdf')
+    fig.savefig(outpath)
+    plt.close(fig)
+    print(f'File saved: {outpath}')
+
+
 def make_comparison(path_central, path_private, region, tag, distribution='mjj'):
     df_central = uproot.open(path_central)[region].pandas.df()
     df_private = uproot.open(path_private)[region].pandas.df()
@@ -64,13 +125,11 @@ def make_comparison(path_central, path_private, region, tag, distribution='mjj')
     # Merge dataframes based on run,event,lumi
     merged_df = df_central.merge(df_private, on=['run', 'event', 'lumi'], suffixes=['_central', '_private'])
 
-    mjj_edges = [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.]
+    # Determine sample year
+    year = re.findall('201\d', path_central)[0]
 
-    central_weights = merged_df['weight_total_central']
-    private_weights = merged_df['weight_total_private']
-
-    h_central, _ = np.histogram(merged_df[f'{distribution}_central'], bins=mjj_edges, weights=central_weights)
-    h_private, _ = np.histogram(merged_df[f'{distribution}_private'], bins=mjj_edges, weights=private_weights)
+    h_central, _ = np.histogram(merged_df[f'{distribution}_central'], bins=BINNINGS[distribution])
+    h_private, _ = np.histogram(merged_df[f'{distribution}_private'], bins=BINNINGS[distribution])
     # Scale with xs & lumi
     def scale_xs_lumi(h, tag):
         return h * XSECTIONS[tag] * 41.5 * 1e3 / SUMW[tag] 
@@ -87,14 +146,14 @@ def make_comparison(path_central, path_private, region, tag, distribution='mjj')
     yerr_private = w2err(h_private, h_private)
 
     fig, ax, rax = fig_ratio()
-    hep.histplot(h_central, ax=ax, bins=mjj_edges, yerr=yerr_central, histtype='step', label='Central')
-    hep.histplot(h_private, ax=ax, bins=mjj_edges, yerr=yerr_private, histtype='step', label='Private')
+    hep.histplot(h_central, bins=BINNINGS[distribution], ax=ax, yerr=yerr_central, histtype='step', label='Central')
+    hep.histplot(h_private, bins=BINNINGS[distribution], ax=ax, yerr=yerr_private, histtype='step', label='Private')
 
     ax.legend(title='Production')
     ax.set_yscale('log')
     ax.set_ylim(1e0,1e4)
     ax.set_ylabel('Events')
-    ax.set_xlabel(r'$M_{jj} \ (GeV)$')
+    ax.set_xlabel(XLABELS[distribution])
     
     ax.text(0.,1.,LABELS[tag],
         fontsize=14,
@@ -103,7 +162,7 @@ def make_comparison(path_central, path_private, region, tag, distribution='mjj')
         transform=ax.transAxes
     )
 
-    ax.text(1.,1.,'2017',
+    ax.text(1.,1.,year,
         fontsize=14,
         ha='right',
         va='bottom',
@@ -118,71 +177,97 @@ def make_comparison(path_central, path_private, region, tag, distribution='mjj')
         'color':'k',
     }
 
+    xedges = BINNINGS[distribution]
+
     r = h_central / h_private
-    mjj_centers = 0.5 * (mjj_edges + np.roll(mjj_edges, -1))[:-1]
+    mjj_centers = 0.5 * (xedges + np.roll(xedges, -1))[:-1]
 
     rax.plot(mjj_centers, r, **data_err_opts)
 
     rax.grid(True)
     rax.set_ylim(0.9,1.1)
     rax.set_ylabel('Central / Private')
-    rax.set_xlabel(r'$M_{jj} \ (GeV)$')
+    rax.set_xlabel(XLABELS[distribution])
 
-    outdir = './output/10May21/with_event_match'
+    outdir = './output/11May21/with_event_match'
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    outpath = pjoin(outdir, f'{tag}_{distribution}_2017.pdf')
+    outpath = pjoin(outdir, f'{tag}_{distribution}_{year}.pdf')
     fig.savefig(outpath)
     plt.close(fig)
     print(f'File saved: {outpath}')
 
 def main():
-    treepaths = {
-        'ewk_zvv' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKZ2Jets_ZToNuNu_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKZ2Jets_ZToNuNu_M-50_withDipoleRecoil-mg_2017.root',
-            'region': 'sr_vbf',
-        },
-        'ewk_zmm' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_2017.root',
-            'region' : 'cr_2m_vbf',
-        },
-        'ewk_zee' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_2017.root',
-            'region' : 'cr_2e_vbf',
-        },
-        'ewk_wminusmv' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'region' : 'cr_1m_vbf',
-        },
-        'ewk_wplusmv' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'region' : 'cr_1m_vbf',
-        },
-        'ewk_wminusev' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'region' : 'cr_1e_vbf',
-        },
-        'ewk_wplusev' : {
-            'path_central': './input/trees_10May21_EWK_V/central/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'path_private': './input/trees_10May21_EWK_V/private/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_2017.root',
-            'region' : 'cr_1e_vbf',
-        },
-    }
-
-    tags  = list(treepaths.keys())
-    for tag in tags:
-        make_comparison(
-            treepaths[tag]['path_central'],
-            treepaths[tag]['path_private'],
-            treepaths[tag]['region'],
-            tag=tag
-        )
+    treetag = 'trees_UL_11May21'
+    for year in [2017,2018]:
+        treepaths = {
+            'ewk_zvv' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKZ2Jets_ZToNuNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKZ2Jets_ZToNuNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'region': 'sr_vbf',
+            },
+            'ewk_zmm' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_{year}.root',
+                'region' : 'cr_2m_vbf',
+            },
+            'ewk_zee' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKZ2Jets_ZToLL_M-50_withDipoleRecoil-mg_{year}.root',
+                'region' : 'cr_2e_vbf',
+            },
+            'ewk_wminusmv' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'region' : 'cr_1m_vbf',
+            },
+            'ewk_wplusmv' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'region' : 'cr_1m_vbf',
+            },
+            'ewk_wminusev' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKWMinus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'region' : 'cr_1e_vbf',
+            },
+            'ewk_wplusev' : {
+                'path_central': f'./input/{treetag}/central/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'path_private': f'./input/{treetag}/private/tree_EWKWPlus2Jets_WToLNu_M-50_withDipoleRecoil-mg_{year}.root',
+                'region' : 'cr_1e_vbf',
+            },
+        }
+    
+        distributions = [
+            'leadak4_ptnano',
+            'leadak4_eta',
+            'trailak4_ptnano',
+            'trailak4_eta',
+            'recoil_pt_nano',
+            'recoil_phi_nano',
+        ]
+    
+        tags  = list(treepaths.keys())
+        for tag in tags:
+            if year == 2018 and tag == 'ewk_zvv':
+                continue
+            for distribution in distributions:
+                # Compare the event-matched distributions
+                make_comparison(
+                    treepaths[tag]['path_central'],
+                    treepaths[tag]['path_private'],
+                    treepaths[tag]['region'],
+                    tag=tag,
+                    distribution=distribution,
+                )
+                # Plot the difference between the two quantities
+                event_by_event_comparison(
+                    treepaths[tag]['path_central'],
+                    treepaths[tag]['path_private'],
+                    treepaths[tag]['region'],
+                    tag=tag,
+                    distribution=distribution
+                )
 
 if __name__ == '__main__':
     main()
--- bucoffea/plot/studies/15Apr21_ReReco_UL/central_vs_private_nano/compare_samples.py
+++ bucoffea/plot/studies/15Apr21_ReReco_UL/central_vs_private_nano/compare_samples.py
@@ -20,9 +20,10 @@ pjoin = os.path.join
 warnings.filterwarnings('ignore')
 
 BINNINGS = {
-    'mjj' : hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500.]),
-    'ak4_pt0' : hist.Bin('jetpt',r'Leading AK4 jet $p_{T}$ (GeV)',list(range(80,600,20)) + list(range(600,1000,20)) ),
-    'ak4_pt1' : hist.Bin('jetpt',r'Trailing AK4 jet $p_{T}$ (GeV)',list(range(40,600,20)) + list(range(600,1000,20)) ),
+    'mjj_nano' : hist.Bin('mjj', r'$M_{jj} \ (GeV)$', [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500.]),
+    'ak4_pt0_nano' : hist.Bin('jetpt',r'Leading AK4 jet $p_{T}$ (GeV)',list(range(80,600,20)) + list(range(600,1000,20)) ),
+    'ak4_pt1_nano' : hist.Bin('jetpt',r'Trailing AK4 jet $p_{T}$ (GeV)',list(range(40,600,20)) + list(range(600,1000,20)) ),
+    'recoil_pt_nano' : hist.Bin('recoil', r'Recoil (GeV)', [ 250,  280,  310,  340,  370,  400,  430,  470,  510, 550,  590,  640,  690,  740,  790,  840,  900,  960, 1020, 1090, 1160, 1250, 1400])
 }
 
 TAGS = {
@@ -49,7 +50,7 @@ def preprocess(h, acc, distribution='mjj'):
 
     return h
 
-def compare_samples(acc_dict, distribution='mjj', years=[2017]):
+def compare_samples(acc_dict, distribution='mjj', years=[2017,2018]):
     '''Compare samples from private and central Nano production.'''
     histos = {}
     for key, acc in acc_dict.items():
@@ -72,6 +73,8 @@ def compare_samples(acc_dict, distribution='mjj', years=[2017]):
             ('ewk_zvv', re.compile(f'EWKZ2Jets.*ZToNuNu.*{year}'), 'sr_vbf_no_veto_all'),
         ]
         for tag, regex, region in datasets:
+            if year == 2018 and tag == 'ewk_zvv':
+                continue
             h_private = histos['private'].integrate('dataset', regex).integrate('region', region)        
             h_central = histos['central'].integrate('dataset', regex).integrate('region', region)
 
@@ -116,7 +119,7 @@ def compare_samples(acc_dict, distribution='mjj', years=[2017]):
                 rax.set_ylim(0.5,1.5)
             rax.set_ylabel('Private / Central')
 
-            outdir = './output/10May21'
+            outdir = './output/11May21_nano'
             if not os.path.exists(outdir):
                 os.makedirs(outdir)
             outpath = pjoin(outdir, f'private_vs_central_{tag}_{distribution}_{year}.pdf')
@@ -126,8 +129,8 @@ def compare_samples(acc_dict, distribution='mjj', years=[2017]):
 
 def main():
     acc_dict = {
-        'private' : dir_archive( bucoffea_path('submission/merged_2021-05-10_vbfhinv_ULv8_05Feb21_EWK_V') ),
-        'central' : dir_archive( bucoffea_path('submission/merged_2021-05-10_vbfhinv_ULv8_05Feb21_central_EWK_V') ),
+        'private' : dir_archive( bucoffea_path('submission/merged_2021-05-11_vbfhinv_ULv8_05Feb21_EWK_V_pureNANO') ),
+        'central' : dir_archive( bucoffea_path('submission/merged_2021-05-11_vbfhinv_ULv8_05Feb21_central_EWK_V_pureNANO') ),
     }
     
     for acc in acc_dict.values():
@@ -136,11 +139,13 @@ def main():
         acc.load('nevents')
 
     distributions = [
-        'mjj', 
+        'mjj_nano', 
         'ak4_eta0', 
         'ak4_eta1',
-        # 'ak4_pt0',
-        # 'ak4_pt1',
+        'ak4_pt0_nano',
+        'ak4_pt1_nano',
+        'recoil_pt_nano',
+        'recoil_phi_nano',
     ]
 
     for distribution in distributions:
