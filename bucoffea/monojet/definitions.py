544098f181b9675a536115a58320d0fea60ef223
Alp Akpinar <aakpinar@bu.edu>
2021-06-24 09:57:43 -0500

    Use unsmeared jet pt while calculating b-veto weights

--- bucoffea/monojet/definitions.py
+++ bucoffea/monojet/definitions.py
@@ -497,6 +497,11 @@ def setup_candidates(df, cfg):
         }
         ak4.add_attributes(**kwargs)
 
+    if not df['is_data']:
+        ak4.add_attributes(jercorr=df['Jet_corr_JER'])
+
+    ak4 = ak4[ak4.looseId]
+
     # Before cleaning, apply HEM veto
     hem_ak4 = ak4[ (ak4.pt>30) &
         (-3.0 < ak4.eta) &
