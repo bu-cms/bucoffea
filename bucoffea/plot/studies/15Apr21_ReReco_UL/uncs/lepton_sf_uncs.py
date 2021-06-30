abf0f3b8c2b85dca16d203b1cd8d9f1ff3317478
Alp Akpinar <aakpinar@bu.edu>
2021-06-30 15:33:49 -0400

    Move the electron trigger SF uncs to different script

--- bucoffea/plot/studies/15Apr21_ReReco_UL/uncs/lepton_sf_uncs.py
+++ bucoffea/plot/studies/15Apr21_ReReco_UL/uncs/lepton_sf_uncs.py
@@ -38,76 +38,6 @@ COLOR_CODE_FOR_VETOWEIGHTS = {
     'tau_id.*' : 5 
 }
 
-def ele_trig_sf_uncs(acc, outtag, dataset, region, distribution='mjj_ele_trig_weight'):
-    acc.load(distribution)
-    h = acc[distribution]
-
-    h = merge_extensions(h, acc)
-    scale_xs_lumi(h)
-    h = merge_datasets(h)
-
-    if 'mjj' in distribution:
-        mjj_bins = [200., 400., 600., 900., 1200., 1500., 2000., 2750., 3500., 5000.]
-        mjj_ax = hist.Bin('mjj', r'$M_{jj} \ (GeV)$', mjj_bins)
-        h = h.rebin('mjj', mjj_ax)            
-
-    h = h.integrate('region', region)
-
-    outdir = f'./output/{outtag}'
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-
-    data_err_opts = {
-        'linestyle':'none',
-        'marker': '.',
-        'markersize': 10.,
-    }
-
-    for year in [2017, 2018]:
-        _h = h.integrate('dataset', re.compile(f'{dataset}.*{year}'))
-        fig, ax, rax = fig_ratio()
-        hist.plot1d(_h, ax=ax, overlay='variation')
-
-        ax.text(0., 1., pretty_dataset_label[region][dataset].format(year=year),
-            fontsize=14,
-            ha='left',
-            va='bottom',
-            transform=ax.transAxes
-        )
-
-        ax.set_yscale('log')
-        ax.set_ylim(1e-2,1e6)
-
-        hist.plotratio(
-            _h.integrate('variation', 'up'),
-            _h.integrate('variation', 'nom'),
-            ax=rax,
-            unc='num',
-            label='Up',
-            error_opts=data_err_opts
-        )
-
-        hist.plotratio(
-            _h.integrate('variation', 'down'),
-            _h.integrate('variation', 'nom'),
-            ax=rax,
-            unc='num',
-            label='Down',
-            error_opts=data_err_opts,
-            clear=False
-        )
-
-        rax.legend(ncol=2)
-        rax.grid(True)
-        rax.set_ylim(0.95,1.05)
-        rax.set_ylabel('Variation')
-
-        outpath = pjoin(outdir, f'{dataset}_ele_trigsf_uncs_{year}.pdf')        
-        fig.savefig(outpath)
-        plt.close(fig)
-
-        print(f'File saved: {outpath}')
-
 def plot_lepton_sf_uncertainties(acc, outtag, sftag, region, dataset, outrootfile):
     '''Plot lepton ID/ISO/RECO SF uncertainties as a function of mjj.'''
     distribution = f'mjj_{sftag}'
@@ -140,7 +70,7 @@ def plot_lepton_sf_uncertainties(acc, outtag, sftag, region, dataset, outrootfil
         fig, ax, rax = fig_ratio()
         hist.plot1d(_h,ax=ax,overlay='variation')
         
-        ax.text(0., 1., pretty_dataset_label[dataset].format(year=year),
+        ax.text(0., 1., pretty_dataset_label[region][dataset].format(year=year),
             fontsize=14,
             ha='left',
             va='bottom',
@@ -317,21 +247,6 @@ def main():
     acc.load('nevents')
 
     outtag = re.findall('merged_.*', inpath)[0].replace('/','')
-
-    datasets_regions = [
-        ('WJetsToLNu', 'cr_1e_vbf'),
-        ('DYJetsToLL', 'cr_2e_vbf'),
-    ]
-
-    try:
-        for dataset, region in datasets_regions:
-            ele_trig_sf_uncs(acc, outtag,
-                dataset=dataset,
-                region=region
-            )
-    except KeyError:
-        print('INFO: Skipping electron trigger SF')
-        pass
 
     # Lepton ID / ISO SF
     lepton_sf_uncs = {
