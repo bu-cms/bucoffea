35fc24a9d43423f5bec50a1d1559bada1318043e
Siqi <yuansiqi0114@hotmail.com>
2021-09-08 12:56:59 -0500

    fixup

--- bucoffea/monojet/monojetProcessor.py
+++ bucoffea/monojet/monojetProcessor.py
@@ -246,13 +246,13 @@ class monojetProcessor(processor.ProcessorABC):
 
         # VBF veto
         diak4 = ak4[:,:2].distincts()
-        leadak4_pt_eta = (diak4.i0.pt > cfg.SELECTION.SIGNAL.LEADAK4.PT) & (np.abs(diak4.i0.eta) < cfg.SELECTION.SIGNAL.LEADAK4.ETA)
-        trailak4_pt_eta = (diak4.i1.pt > cfg.SELECTION.SIGNAL.TRAILAK4.PT) & (np.abs(diak4.i1.eta) < cfg.SELECTION.SIGNAL.TRAILAK4.ETA)
+        leadak4_pt_eta = (diak4.i0.pt > 80) & (np.abs(diak4.i0.eta) < 4.7)
+        trailak4_pt_eta = (diak4.i1.pt > 40) & (np.abs(diak4.i1.eta) < 4.7)
         hemisphere = (diak4.i0.eta * diak4.i1.eta < 0).any()
         has_track0 = np.abs(diak4.i0.eta) <= 2.5
         has_track1 = np.abs(diak4.i1.eta) <= 2.5
-        leadak4_id = diak4.i0.tightId & (has_track0*((diak4.i0.chf > cfg.SELECTION.SIGNAL.LEADAK4.CHF) & (diak4.i0.nhf < cfg.SELECTION.SIGNAL.LEADAK4.NHF)) + ~has_track0)
-        trailak4_id = has_track1*((diak4.i1.chf > cfg.SELECTION.SIGNAL.TRAILAK4.CHF) & (diak4.i1.nhf < cfg.SELECTION.SIGNAL.TRAILAK4.NHF)) + ~has_track1
+        leadak4_id = diak4.i0.tightId & (has_track0*((diak4.i0.chf > 0.1) & (diak4.i0.nhf < 0.8)) + ~has_track0)
+        trailak4_id = has_track1*((diak4.i1.chf > 0.1) & (diak4.i1.nhf < 0.8)) + ~has_track1
         pass_vbf = True;
         pass_vbf = pass_vbf & (diak4.counts>0)
         pass_vbf = pass_vbf & (leadak4_pt_eta.any())
@@ -260,9 +260,9 @@ class monojetProcessor(processor.ProcessorABC):
         pass_vbf = pass_vbf & (hemisphere)
         pass_vbf = pass_vbf & (leadak4_id.any())
         pass_vbf = pass_vbf & (trailak4_id.any())
-        pass_vbf = pass_vbf & (diak4.mass.max() > cfg.SELECTION.SIGNAL.DIJET.SHAPE_BASED.MASS)
-        pass_vbf = pass_vbf & (dphi(diak4.i0.phi.min(), diak4.i1.phi.max()) < cfg.SELECTION.SIGNAL.DIJET.SHAPE_BASED.DPHI)
-        pass_vbf = pass_vbf & (np.abs(diak4.i0.eta - diak4.i1.eta).max() > cfg.SELECTION.SIGNAL.DIJET.SHAPE_BASED.DETA)
+        pass_vbf = pass_vbf & (diak4.mass.max() > 200)
+        pass_vbf = pass_vbf & (dphi(diak4.i0.phi.min(), diak4.i1.phi.max()) < 1.5)
+        pass_vbf = pass_vbf & (np.abs(diak4.i0.eta - diak4.i1.eta).max() > 1)
 
         selection = processor.PackedSelection()
 
